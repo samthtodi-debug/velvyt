10332bbc01da65ebf0b8a03a40954f1840bbc9b4
samthtodi-debug <samth.todi@gmail.com>
2026-02-05 22:47:20 +0530

    style: glassmorphism for star border button

--- client/src/components/StarBorder.tsx
+++ client/src/components/StarBorder.tsx
@@ -43,7 +43,7 @@ const StarBorder = <T extends React.ElementType = 'button'>({
                     animationDuration: speed
                 }}
             ></div>
-            <div className="relative z-1 bg-gradient-to-b from-black to-gray-900 border border-gray-800 text-white text-center text-[16px] py-[16px] px-[26px] rounded-[20px]">
+            <div className="relative z-1 bg-black/20 backdrop-blur-xl border border-white/10 text-white text-center text-[16px] py-[16px] px-[26px] rounded-[20px]">
                 {children}
             </div>
         </Component>
